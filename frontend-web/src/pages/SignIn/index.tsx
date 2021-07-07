88fc481105f453288d36bde4df55c6954d000cea
Lucas Luis Ribeiro <lucas.luisribeiro@gmail.com>
2021-07-06 22:54:57 -0300

    Componentes de botao e input

--- frontend-web/src/pages/SignIn/index.tsx
+++ frontend-web/src/pages/SignIn/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
-import { FiLogIn } from 'react-icons/fi';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 
 import logoImg from '../../assets/logo.svg';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 import { Container, Content, Backgroud } from './styles';
 
@@ -13,10 +16,15 @@ const SignIn: React.FC = () => (
       <form>
         <h1>Faça seu logon</h1>
 
-        <input type="text" placeholder="Email" />
-        <input type="password" placeholder="Senha" />
+        <Input name="email" icon={FiMail} type="text" placeholder="Email" />
+        <Input
+          name="password"
+          icon={FiLock}
+          type="password"
+          placeholder="Senha"
+        />
 
-        <button type="submit">Entrar</button>
+        <Button type="submit">Entrar</Button>
 
         <a href="forgt">Esqueci minha senha</a>
       </form>
