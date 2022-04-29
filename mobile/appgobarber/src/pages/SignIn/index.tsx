5befc9d7449d6568692b93bee846052aa7c29758
Lucas Luis Ribeiro <lucas.luisribeiro@gmail.com>
2022-04-28 21:25:03 -0300

    Input e Button

--- mobile/appgobarber/src/pages/SignIn/index.tsx
+++ mobile/appgobarber/src/pages/SignIn/index.tsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import {Image} from 'react-native';
 
-import {Container, Title} from './styles';
+import Input from '../../components/Input/Index';
+import Button from '../../components/Button/Index';
+
 import logoImg from '../../assets/logo.png';
+import {Container, Title} from './styles';
 
 const SignIn: React.FC = () => {
   return (
     <Container>
       <Image source={logoImg} />
       <Title>Faça seu logon</Title>
+
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+
+      <Button
+        onPress={() => {
+          console.log('teste');
+        }}>
+        Entrar
+      </Button>
     </Container>
   );
 };
